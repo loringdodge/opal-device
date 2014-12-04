4b6f50c9e06776650672732cd6eef662385abaf4
Caroline Wong <carowong5@gmail.com>
2014-12-04 12:04:59 -0800

    connects to instagram and returns objects from user's feed

--- client/app.js
+++ client/app.js
@@ -2,11 +2,12 @@
 $(document).ready(function() {
 	OAuth.initialize('mjBY4FTkZ4yHocgHANa2ix7-m5w');
 	
-	var provider = 'twitter';
-
+	var provider = 'instagram';
+	// https://api.instagram.com/v1/users/[USER ID]/media/recent/?client_id=[CLIENT ID]
+	// https://api.instagram.com/v1/users/3/media/recent/?access_token=ACCESS-TOKEN
 	OAuth.popup(provider)
 	.done(function(result) {
-	    result.get('https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=169686021&count=200').done(function(data) {
+	    result.get('https://api.instagram.com/v1/users/217257560/media/recent/?client_id=0818d423f4be4da084f5e4b446457044&count=1').done(function(data) {
 			console.log(JSON.stringify(data));
 		}).fail(function(err) {
 		  // with err

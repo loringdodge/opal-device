eeeaeea6007701591fd64066c4a7d87f44582ee7
Phung.Doug@gmail.com <phung.doug@gmail.com>
2014-12-04 22:03:57 -0800

    jorge helped set up index routing!

--- server/routes.js
+++ server/routes.js
@@ -7,12 +7,7 @@
 var errors = require('./components/errors');
 
 module.exports = function(app) {
-  // Insert routes below
+  // Insert routes here, guys!
   app.use('/api/instagram', require('./api/instagram'));
-  
-  // All other routes should redirect to the index.html
-  app.route('/')
-    .get(function(req, res) {
-      res.sendFile('../client/index.html');
-    });
+
 };
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var sentiment = require('./api/router');
 var config = require('./config/environment/development');
+var path = require('path');
 
 var app = express();
 
@@ -9,15 +10,9 @@ mongoose.connect(config.mongo.uri, config.mongo.options);
 
 if (config.seedDB) { require('./config/seed'); }
 
-//require('./api/instagram/instagramModel.js');
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
-
 require('./routes')(app);
 
+app.use(express.static(path.join(__dirname, '/../client')));
 
 sentiment.getSentiment("I love turtles!");
 

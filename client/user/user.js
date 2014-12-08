ed3b9575b08a2f8e5b8534798b11bf4b4c97fb2f
Caroline Wong <carowong5@gmail.com>
2014-12-08 12:17:15 -0800

    can enter a user and location.returns objects in console

--- client/user/user.js
+++ client/user/user.js
@@ -3,7 +3,6 @@ angular.module('omnigrahm.user', [])
 
   $scope.dates, $scope.positiveData, $scope.negativeData;
   var updateData = function(data) {
-    console.log('CALLED GET DATA');
     var result = getData(data);
     var dateData = result[0];
     var positiveData = result[1];
@@ -12,11 +11,8 @@ angular.module('omnigrahm.user', [])
     $scope.chartData.x = $scope.chartData.x.concat(dateData);
     $scope.chartData.positive = $scope.chartData.positive.concat(positiveData);
     $scope.chartData.negative = $scope.chartData.negative.concat(negativeData);
-    console.log($scope.chartData.x);
     showGraph();
-    // showImages(images);
   };
-//
   $scope.chartData = {
     x: ['x'],
     positive: ['positive'],
@@ -24,7 +20,6 @@ angular.module('omnigrahm.user', [])
   };
 
   var showGraph = function() {
-    console.log('something happens');
     $scope.chart = c3.generate({
       bindto: '#userChart',
       data: {
@@ -58,33 +53,23 @@ angular.module('omnigrahm.user', [])
     });
   };
 
-  // var showImages = function(images) {
-  //   for (var i = 0; i < Things.length; i++) {
-  //     Things[i]
-  //   };
-  // };
-
-  $scope.getUserFeed = function(userId) {
+  $scope.getUserFeed = function(username) {
 		OAuth.initialize('mjBY4FTkZ4yHocgHANa2ix7-m5w');
 		var provider = 'instagram';
-		userId = userId || 217257560;
 
-		OAuth.popup(provider)
-		.done(function(result) {
-			console.log(result.user.id);
-        //post to api/instagram -fix so only queries for user
-        $http.get('/api/instagram') //, {params: { user_id: result.user.id } }
-			  .success(function(data, status, headers, config) {
-          //gets stuff back from the user
-          // $scope.data = data;
-			  	console.log('gets objects back');
-			  	console.log(data);
-          updateData(data);
-			  })
-        //error on /api/instagram query
-			  .error(function(data, status, headers, config) {
-          //query instagram for user stuff
-          result.get('https://api.instagram.com/v1/users/' + userId + '/media/recent/?client_id=0818d423f4be4da084f5e4b446457044&count=5')
+    console.log(username);
+
+    var userId = userId || 217257560;
+
+    OAuth.popup(provider)
+    .done(function(result) {
+
+    if (username) {
+
+      result.get('https://api.instagram.com/v1/users/search?q='+ username +'&client_id=0818d423f4be4da084f5e4b446457044')
+      .then(function(userObj) {
+        userId = userObj.data[0].id;
+        result.get('https://api.instagram.com/v1/users/' + userId + '/media/recent/?client_id=0818d423f4be4da084f5e4b446457044&count=40')
           .done(function(userObjects) {
             //go through sentiment api
             // console.log(userObjects.data); //array of objects returned
@@ -121,11 +106,69 @@ angular.module('omnigrahm.user', [])
                       // or server returns response with an error status.
                   })
                 })                  
-              }); //end of for loop
+              });
           });
-			  	console.log('not found');
-			    // log error
-			  });
+      })
+      //can't get userid
+      .fail(function(err) {
+        console.log(err);
+      })
+    } //end of if
+        
+        //post to api/instagram -fix so only queries for user
+     //    $http.get('/api/instagram') //, {params: { user_id: result.user.id } }
+			  // .success(function(data, status, headers, config) {
+     //      //gets stuff back from the user
+     //      // $scope.data = data;
+			  // 	console.log('gets objects back');
+			  // 	console.log(data);
+     //      updateData(data);
+			  // })
+     //    //error on /api/instagram query
+			  // .error(function(data, status, headers, config) {
+     //      //query instagram for user stuff
+     //      result.get('https://api.instagram.com/v1/users/' + userId + '/media/recent/?client_id=0818d423f4be4da084f5e4b446457044&count=5')
+     //      .done(function(userObjects) {
+     //        //go through sentiment api
+     //        // console.log(userObjects.data); //array of objects returned
+     //          /*
+     //              for each object returned. getSentiment() returns string. getPosNeg() returns sentiment object.
+     //              push to the userObjects[i]. post to /api/instagram the whole object
+     //          */
+     //          // var objectToSave;
+     //          // for (var i = 0; i < userObjects.data.length; i++) {
+     //            userObjects.data.forEach(function(obj) {
+     //            // objectToSave = userObjects.data[i];
+     //            // objectToSave = obj;
+     //            console.log(obj);
+     //            var caption = getCaptionString(obj);
+
+     //            //api call 
+     //            var string = caption.replace(' ', '%20');
+     //            $http.get('https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=' + string, {
+     //              headers: { 'X-Mashape-Key': 'bZKtaWEZMmmshwTi4qO4XJhxvNfCp13uY3yjsnYweDF3s3S2Bw'}
+     //            })
+     //            .success(function(data) {
+     //              obj['sentiment'] = data;
+
+     //              //post to /api/instagram individual objects
+     //              $http.post('/api/instagram', JSON.stringify(obj))
+     //              .success(function(data, status, headers, config) {
+     //                console.log('posted!!!');
+     //                // console.log(data);
+     //                // this callback will be called asynchronously
+     //                // when the response is available
+     //              })
+     //              .error(function(data, status, headers, config) {
+     //                  // called asynchronously if an error occurs
+     //                  // or server returns response with an error status.
+     //              })
+     //            })                  
+     //          }); //end of for loop
+     //      });
+			  // 	console.log('not found');
+			  //   // log error
+			  // });
         //end of error
 
 		}).fail(function(err) {

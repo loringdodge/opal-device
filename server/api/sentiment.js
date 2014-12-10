e1e79c3041c1992838e131d8db5459cea5950f26
Brian Scoles <brianscoles@gmail.com>
2014-12-10 11:19:13 -0800

    bit more refactoring and commenting for sentimentRouter

--- server/api/sentiment.js
+++ server/api/sentiment.js
@@ -6,30 +6,33 @@ var express = require('express');
 var sentimentRouter = express.Router();
 var analyzeSentiment = require('sentiment');
 
+
+//this route expects an array of strings, and returns an array with an object for 
+//each input string.
+// REQ: ["love","hate"]
+// RES: [{"message": "love", "score": 3 }, {"message": "hate", "score": -3 }]
+
 sentimentRouter.get('/', function(req,res){
   var messages = JSON.parse(req.query.messages);
-  
-  // option A: return object, where keys are input strings and values are their sentiment score
+  var sentiments = messages.map(function(message){
+    return { message: message, score: analyzeSentiment(message).score };
+  })
+  res.json(sentiments);
+} );
+
+
+module.exports = sentimentRouter;
+
+
+
+
+  // option B: return object, where keys are input strings and values are their sentiment score
   // var sentiments = {};
   // messages.forEach(function(message){
   //   sentiments[message] = analyzeSentiment(message).score;
   // });
 
-  //option B: return array of tuples, where: [[inputString, sentimentScore], [inputString, sentimentScore], ...]
+  //option C: return array of tuples, where: [[inputString, sentimentScore], [inputString, sentimentScore], ...]
   // var sentiments = messages.map(function(message){
   //   return [message,analyzeSentiment(message).score];
   // })
-
-  //option C: return array of objects, where [{string: inputString, score: sentimentScore}...]
-  var sentiments = messages.map(function(message){
-    return {
-      message: message,
-      score: analyzeSentiment(message).score
-    };
-  })
-
-  res.json(sentiments)
-} );
-
-
-module.exports = sentimentRouter;

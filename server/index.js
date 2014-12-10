a2cffec5bf1118875fd55afdf17c31c392f98f16
Brian Scoles <brianscoles@gmail.com>
2014-12-10 12:02:38 -0800

    fix syntax in bower.json

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ var bodyParser = require('body-parser');
 var Q = require('q'); 
 //var instagramRouter = require("./api/instagram");
 var sentimentRouter = require("./api/sentiment");
-var jsonParser = bodyParser.json()
-
+var jsonParser = bodyParser.json();
+var crontab = require('node-crontab');
 
 mongoose.connect(config.mongo.uri, config.mongo.options);
 
@@ -26,7 +26,13 @@ app.use(bodyParser.urlencoded({ extended: false }))    //QUESTION: is this in th
    .listen(config.port);
 console.log("server listening on port " + config.port);
 
+var announceTime = function(){
+  console.log("It's been 1 minute!")
+};
 
+var job1 = crontab.scheduleJob("* * * * *", function(){ //This will call this function every 1 minute
+    announceTime();
+});
 
 
 

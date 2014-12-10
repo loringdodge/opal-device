e2982c96b139071adc5a954877853c83ee583e76
Brian Scoles <brianscoles@gmail.com>
2014-12-10 11:14:26 -0800

    refactor sentimentRouter return value

--- server/api/sentiment.js
+++ server/api/sentiment.js
@@ -4,15 +4,30 @@
 
 var express = require('express');
 var sentimentRouter = express.Router();
-var sentiment = require('sentiment');
+var analyzeSentiment = require('sentiment');
 
 sentimentRouter.get('/', function(req,res){
   var messages = JSON.parse(req.query.messages);
-  console.log("messages:",messages);
+  
+  // option A: return object, where keys are input strings and values are their sentiment score
+  // var sentiments = {};
+  // messages.forEach(function(message){
+  //   sentiments[message] = analyzeSentiment(message).score;
+  // });
+
+  //option B: return array of tuples, where: [[inputString, sentimentScore], [inputString, sentimentScore], ...]
+  // var sentiments = messages.map(function(message){
+  //   return [message,analyzeSentiment(message).score];
+  // })
+
+  //option C: return array of objects, where [{string: inputString, score: sentimentScore}...]
   var sentiments = messages.map(function(message){
-    return sentiment(message);
-  });
-  console.log("sentiments:", sentiments)
+    return {
+      message: message,
+      score: analyzeSentiment(message).score
+    };
+  })
+
   res.json(sentiments)
 } );
 

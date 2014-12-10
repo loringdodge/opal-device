f0b8a2eca2d7a29e7ab6e0477435009440b82f58
Loring Dodge <loringdodge@yahoo.com>
2014-12-09 19:31:13 -0800

    Refactor server code

--- server/api/router.js
+++
@@ -1,8 +0,0 @@
-var unirest = require('unirest');
-module.exports.getSentiment =  function(string){
-	unirest.get("https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=" + string)
-	.header("X-Mashape-Key", "bZKtaWEZMmmshwTi4qO4XJhxvNfCp13uY3yjsnYweDF3s3S2Bw")
-	.end(function (result) {
-	  console.log(result.body);
-	});
-};
--- server/api/user/userModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var UserSchema = new mongoose.Schema({
-  //username
-  username: String,
-  userid: Number
-});
-
-module.exports = mongoose.model('User', UserSchema);

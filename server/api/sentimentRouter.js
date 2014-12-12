b778b00cc4d64c8d6654ec0a2d9edfaf85b7f62c
thejsj <jorge.silva@thejsj.com>
2014-12-12 14:22:28 -0800

    Add demo. Clean up

--- server/api/sentimentRouter.js
+++ server/api/sentimentRouter.js
@@ -1,38 +1,39 @@
-//sentiment.js
-
-'use strict'; 
+/*jshint node:true */
+'use strict';
 
 var express = require('express');
 var sentimentRouter = express.Router();
 var analyzeSentiment = require('sentiment');
 
 
-//this route expects an array of strings, and returns an array with an object for 
+//this route expects an array of strings, and returns an array with an object for
 //each input string.
 // REQ: ["love","hate"]
 // RES: [{"message": "love", "score": 3 }, {"message": "hate", "score": -3 }]
 
-sentimentRouter.get('/', function(req,res){
+sentimentRouter.get('/', function (req, res) {
   var messages = JSON.parse(req.query.messages);
-  var sentiments = messages.map(function(message){
-    return { message: message, score: analyzeSentiment(message).score };
+  var sentiments = messages.map(function (message) {
+    return {
+      message: message,
+      score: analyzeSentiment(message).score
+    };
   })
   res.json(sentiments);
-} );
+});
 
 
 module.exports = sentimentRouter;
 
 
 
+// option B: return object, where keys are input strings and values are their sentiment score
+// var sentiments = {};
+// messages.forEach(function(message){
+//   sentiments[message] = analyzeSentiment(message).score;
+// });
 
-  // option B: return object, where keys are input strings and values are their sentiment score
-  // var sentiments = {};
-  // messages.forEach(function(message){
-  //   sentiments[message] = analyzeSentiment(message).score;
-  // });
-
-  //option C: return array of tuples, where: [[inputString, sentimentScore], [inputString, sentimentScore], ...]
-  // var sentiments = messages.map(function(message){
-  //   return [message,analyzeSentiment(message).score];
-  // })
+//option C: return array of tuples, where: [[inputString, sentimentScore], [inputString, sentimentScore], ...]
+// var sentiments = messages.map(function(message){
+//   return [message,analyzeSentiment(message).score];
+// })
--- server/api/utils.js
+++ server/api/utils.js
@@ -1,4 +1,5 @@
-var cities = require('../../cities.json');
+/*jshint node:true */
+var cities = require('../cities.json');
 var instagram = require('./instagram');
 var sentiment = require('./sentiment');
 var Promise = require('bluebird');
@@ -35,25 +36,27 @@ var utils = {
   //   response.end();
   // },
 
-  getTop30 : function(){
-    console.log("getting top 30!")
+  getTop30: function () {
+    console.log("getting top 30!");
     var i = 0;
 
-    var getNextCity = function(){
+    var getNextCity = function () {
       var city = cities.cities[i++];
       console.log("sending req for " + city.name);
-        utils.getInstagrams(city,100);
-      if(i<cities.cities.length){
-       setTimeout(getNextCity,200);
+      utils.getInstagrams(city, 100);
+      if (i < cities.cities.length) {
+        setTimeout(getNextCity, 200);
       }
-    }
+    };
 
     getNextCity();
 
   },
 
-  addInstagramDataToDb: function(city, messages, originalRes){   
-    var query  = Cities.where({ placeId: city.placeId });
+  addInstagramDataToDb: function (city, messages, originalRes) {
+    var query = Cities.where({
+      placeId: city.placeId
+    });
     var updating = false;
     query.findOneAsync()
       .then(function (cityRecord) {
@@ -61,86 +64,85 @@ var utils = {
           console.log("city exists in DB");
           updating = true;
         } else {
-          console.log("city doesn't yet exist in DB... creating new record now.")
+          console.log("city doesn't yet exist in DB... creating new record now.");
           cityRecord = new Cities({
             name: city.name,
             lat: city.lat,
             lng: city.lng,
             placeId: city.placeId,
-            total_positives : 0,
-            total_negatives : 0,
-            total_neutrals  : 0,
+            total_positives: 0,
+            total_negatives: 0,
+            total_neutrals: 0,
             percent_positive: 0,
             percent_negative: 0,
-            total_searched : 0,
-            photo_urls : {}
+            total_searched: 0,
+            photo_urls: {}
           });
-      }
-      return cityRecord;
-    })
-    .then(function (cityRecord) {
-      //iterate through messages to further populate city record
-      _.each(messages,function(message,key){
-        var instagramId = message.url.split('/')[4];
-        if(key !== "size" && !cityRecord.photo_urls[instagramId]){
-          cityRecord.photo_urls[instagramId] = true;
-          cityRecord.total_searched++;
-          if(message.sentiment>0) cityRecord.total_positives ++;
-          else if(message.sentiment<0) cityRecord.total_negatives ++;
-          else cityRecord.total_neutrals ++;
+        }
+        return cityRecord;
+      })
+      .then(function (cityRecord) {
+        //iterate through messages to further populate city record
+        _.each(messages, function (message, key) {
+          var instagramId = message.url.split('/')[4];
+          if (key !== "size" && !cityRecord.photo_urls[instagramId]) {
+            cityRecord.photo_urls[instagramId] = true;
+            cityRecord.total_searched++;
+            if (message.sentiment > 0) cityRecord.total_positives++;
+            else if (message.sentiment < 0) cityRecord.total_negatives++;
+            else cityRecord.total_neutrals++;
+          }
+          return cityRecord;
+        });
+        cityRecord.percent_positive = cityRecord.total_positives / cityRecord.total_searched;
+        cityRecord.percent_negative = cityRecord.total_negatives / cityRecord.total_searched;
+
+
+        console.log("saving record to DB: " + cityRecord.name + " with " + cityRecord.total_searched + " messages.");
+        if (updating) {
+          if (originalRes) originalRes.status(201).json(cityRecord);
+          return cityRecord.saveAsync();
+        } else {
+          var cityDB = new Cities(cityRecord);
+          if (originalRes) originalRes.status(201).json(cityDB);
+          return cityDB.saveAsync();
         }
       })
-      cityRecord.percent_positive = cityRecord.total_positives / cityRecord.total_searched;
-      cityRecord.percent_negative = cityRecord.total_negatives / cityRecord.total_searched;
-
-      
-      console.log("saving record to DB: " + cityRecord.name + " with " + cityRecord.total_searched + " messages.")
-      if(updating){
-        if(originalRes) originalRes.status(201).json(cityRecord);
-        return cityRecord.saveAsync();
-      } else {
-        var cityDB = new Cities(cityRecord);
-        if(originalRes) originalRes.status(201).json(cityDB);
-        return cityDB.saveAsync();
-      }
-    })
-    .catch(function (err) {
-      console.log('ERROR: ');
-      console.log(err);
-    });
+      .catch(function (err) {
+        console.log('ERROR: ');
+        console.log(err);
+      });
   },
 
-  getInstagrams: function(city,number,originalRes){
+  getInstagrams: function (city, number, originalRes) {
     console.log("getting mass instagrams");
     var storage = [];
-
-      var clientId = '0818d423f4be4da084f5e4b446457044';
-      var apiUrl = 'https://api.instagram.com/v1/media/search?lat=' + city.lat;
-          apiUrl += '&lng=' + city.lng + '&client_id=' + clientId + '&count=300';
-
-      request(apiUrl)
+    var clientId = '0818d423f4be4da084f5e4b446457044';
+    var apiUrl = 'https://api.instagram.com/v1/media/search?lat=' + city.lat;
+    apiUrl += '&lng=' + city.lng + '&client_id=' + clientId + '&count=300';
+    request(apiUrl)
       .then(function (res, body) {
-        if(res.statusCode == 400) {
+        if (res.statusCode == 400) {
           throw new Error('400 error on request');
         }
-        var json = JSON.parse(res[0].body)
-        if(json.data === undefined ) throw new Error ('error with API call');
+        var json = JSON.parse(res[0].body);
+        if (json.data === undefined) throw new Error('error with API call');
         var messages = json.data;
-        var parsedMessages = messages.forEach(function(message){
+        var parsedMessages = messages.forEach(function (message) {
           var text = message.caption ? message.caption.text : "";
           var newMessage = {
             text: text,
             url: message.link,
             sentiment: sentiment(text)
-          }
+          };
           storage.push(newMessage);
-        })
-        utils.addInstagramDataToDb(city,storage,originalRes);
+        });
+        utils.addInstagramDataToDb(city, storage, originalRes);
       })
       .catch(function (err) {
         console.log("Error: " + err);
       });
   }
-}
+};
 
 module.exports = utils;
